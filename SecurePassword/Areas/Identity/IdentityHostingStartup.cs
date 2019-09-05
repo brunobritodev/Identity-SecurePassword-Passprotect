5697b854bce957ea411ba66d9a0119e28e139726
Bruno Brito <bhdebrito@gmail.com>
2019-09-05 19:28:50 -0300

    running version

---
+++ SecurePassword/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SecurePassword.Data;
+
+[assembly: HostingStartup(typeof(SecurePassword.Areas.Identity.IdentityHostingStartup))]
+namespace SecurePassword.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ SecurePassword/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using SecurePassword.Config;
+using System.Threading.Tasks;
+
+namespace SecurePassword
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            Task.WaitAll(DbMigrationHelpers.EnsureSeedData(host));
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
